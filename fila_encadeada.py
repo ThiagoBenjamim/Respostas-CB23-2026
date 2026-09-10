d400f1444eb8754a0a925e629f589b9fa2c573b4
ThiagoBenjamim <thiago08benjamim@gmail.com>
2026-09-10 14:35:28 -0300

    Adição de comentários e correção de erros.

--- fila_encadeada.py
+++ fila_encadeada.py
@@ -2,17 +2,19 @@ from pilha_encadeada import PilhaEncadeada
 
 class FilaEncadeada:
     def __init__(self):
+        '''Inicializa a classe Fila Encadeada contendo uma pilha para a entrada de nodes e uma para a extração'''
 
         self.pilha_entrada = PilhaEncadeada()
         self.pilha_saida = PilhaEncadeada()
 
     def enfileirar(self, data):
-
+        '''Insere o item na pilha de entrada com o método push de complexidade O(1)'''
         self.pilha_entrada.push(data)
 
     def desenfileirar(self):
-    #Pode ser O(1) quando a pilha de saída tiver elementos,
-    #mas quando ela estiver vazia, será necessário a reordenação da pilha de entrada, o que custa O(n)
+        '''Remove o item mais antido da pilha de extração,
+        caso ela esteja vazia, retira os elementos da pilha de entrada e os insere em si mesmo em ordem contrária,
+        (Explicação mais detalhada no documento md)'''
 
         if self.pilha_saida.esta_vazia():
             while not self.pilha_entrada.esta_vazia():
@@ -24,8 +26,9 @@ class FilaEncadeada:
         return self.pilha_saida.pop()
 
     def frente(self):
-    #Pode ser O(1) quando a pilha de saída tiver elementos,
-    #mas quando ela estiver vazia, será necessário a reordenação da pilha de entrada, o que custa O(n)
+        '''Retorna o documento mais antigo da pilha de extração,
+        caso ela esteja vazia, faz o mesmo que o método desenfileirar para preenchela,
+        (Explicação mais detalhada no documento md)'''
 
         if self.pilha_saida.esta_vazia():
             while not self.pilha_entrada.esta_vazia():
@@ -37,27 +40,35 @@ class FilaEncadeada:
         return self.pilha_saida.topo()
 
     def esta_vazia(self):
+        '''Retorna True se os dois métodos esta_vazia() forem True também,
+        como são dois métodos O(1),
+        a complexidade é O(1)'''
 
         return (self.pilha_entrada.esta_vazia() and self.pilha_saida.esta_vazia())
 
     def __len__(self):
+        '''Retorna a soma do comprimento das duas pilhas, assim,
+        tem complexidade O(1) ao chamar dois métodos O(1)'''
 
         return (len(self.pilha_entrada) + len(self.pilha_saida))
 
     def repr(self):
+        '''Primeiro passa todos os itens da pilha de entrada para a pilha de extração, que é uma ação O(N),
+        após isso, '''
 
-        if self.pilha_saida.esta_vazia():
-            while not self.pilha_entrada.esta_vazia():
-                self.pilha_saida.push(self.pilha_entrada.pop())
+        while not self.pilha_entrada.esta_vazia():
+            self.pilha_saida.push(self.pilha_entrada.pop())
 
         if self.pilha_saida.esta_vazia():
             raise IndexError("Fila está vazia.")
 
-        current = self.pilha_saida.header_node
-        text = ""
-        while current.next != None:
-            text += f"{current.data} - "
-            current = current.next
-        text += str(current.data)
+        sub_pilha_saida = self.pilha_saida
+        text = f"{sub_pilha_saida.pop()}"
+        while not sub_pilha_saida.esta_vazia():
+            text += f" - {sub_pilha_saida.pop()}"
         
         return text
+
+fila = FilaEncadeada()
+
+print(fila.repr())
--- pilha_encadeada.py
+++ pilha_encadeada.py
@@ -1,14 +1,21 @@
 class Node:
     def __init__(self, data):
+        '''Inicia a classe Node.'''
         self.data = data
         self.next = None
 
 class PilhaEncadeada:
     def __init__(self):
+        '''Inicia a classe Pilha Encadeada com o header node vazio e um contador de comprimento inicialmente vazio.'''
+
         self.header_node = None
         self.length_counter = 0
 
     def push(self, node_data):
+        '''Insere um node no topo da pilha.
+        Como o node simplesmente assume o valor do header node,
+        que pode ser acessado intantaneamente, e o endereça depois, sua complexidade é:
+        O(1)'''
 
         node = Node(node_data)
 
@@ -22,6 +29,9 @@ class PilhaEncadeada:
         self.length_counter += 1
 
     def pop(self):
+        '''Escolhe o node endereçado pelo header atual como novo header e retorna o substituido,
+        Pelos mesmos motivos do método anterior, sua complexidade é:
+        O(1)'''
 
         if self.header_node == None:
             raise IndexError("Pilha Vazia")
@@ -32,23 +42,36 @@ class PilhaEncadeada:
         return popped.data
 
     def topo(self):
+        '''Simplemente retorna o header node, que pode ser acessado direto,
+        A complexidade é:
+        O(1)'''
+
         if self.header_node == None:
             raise IndexError("Pilha Vazia")
         
         return self.header_node.data
 
     def esta_vazia(self):
+        '''Verifica o contador de comprimento e retorna se é maior que 0
+        Sua complexidade é:
+        O(1)'''
 
-        if self.length_counter == 0:
+        if self.length_counter <= 0:
             return True
         else:
             return False
 
     def __len__(self):
+        '''Simplemente retorna o contador de comprimento,
+        sua complexidade é:
+        O(1)'''
 
         return self.length_counter
 
     def repr(self):
+        '''Como precisa percorrer a pilha inteira pelo menos uma vez,
+        sua complexidade é:
+        O(N)'''
 
         if self.esta_vazia():
             raise IndexError("Pilha vazia.")
@@ -61,3 +84,7 @@ class PilhaEncadeada:
         text += str(current.data)
 
         return text
+
+
+pilha = PilhaEncadeada()
+print(pilha.repr())
